c2c127320e5dc7f315dbe0769fe33842424b89b8
kurt.iamele <kurt.iamele@gmail.com>
2020-08-05 13:21:37 -0500

    fixed /favicon.ico 404 issue

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,9 @@
 const FILES_TO_CACHE = [
     "/",
     "index.html",
-    "/style.css",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png",
+    "/styles.css",
+    "icons/icon-192x192.png",
+    "icons/icon-512x512.png",
     "index.js",
     "db.js"
 ]
